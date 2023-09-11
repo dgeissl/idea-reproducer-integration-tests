0a2531bd917ccd120a56510c306eda919bc20dd3
Daniel Geißler <5918326+dgeissl@users.noreply.github.com>
2023-09-11 10:45:42 +0200

    IDEA-321592 minor fixes to intended setup

--- tests-downstream/src/test/java/itdownstream/DownstreamInfoTest.java
+++ tests-downstream/src/test/java/itdownstream/DownstreamInfoTest.java
@@ -1,13 +1,14 @@
 package itdownstream;
 
+import base.BaseIntegrationTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DownstreamInfoTest {
+public class DownstreamInfoTest extends BaseIntegrationTest {
 
     @Test
-    public void testBase(){
+    public void testDownstream(){
         DownstreamInfo info = new DownstreamInfo();
         assertEquals("I am very basic.", info.getInfo());
     }
